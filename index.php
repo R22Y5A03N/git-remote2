a4e42f321103c66ea2cd276690ac3ed525b02895
R22Y5A03N <ryanahmad220503@gmail.com>
2024-12-22 17:03:06 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>git remote dari local</title>
+</head>
+<body>
+    <h1>git remote</h1>
+    <p>meghubungkan repo dari local ke github</p>
+</body>
+</html>
